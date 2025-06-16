1e0de19c9e149b71db996ce5006ff0a483432f35
skranee <125743202+skranee@users.noreply.github.com>
2025-06-16 14:48:16 +0300

    feat: login form improvements

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -6,12 +6,16 @@ import { useAppSelector } from '@/hooks/useAppSelector.ts';
 import { setSignMode } from '@/store/slices/AuthSlice.ts';
 import { type FormEvent, useState } from 'react';
 import { Input } from '@/components/controls/Input/Input.tsx';
+import { useNavigate } from 'react-router-dom';
+import ArrowLeft from '@/assets/icons/arrow-left.svg';
 
 export const Login = () => {
   const signMode = useAppSelector(state => state.auth.signMode);
 
   const dispatch = useAppDispatch();
   const { signInWithGoogle, signInWithPassword, signUpWithPassword } = useAuth();
+
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -39,43 +43,85 @@ export const Login = () => {
     signUpWithPassword(email, password);
   };
 
+  const goBack = () => {
+    if (signMode) {
+      dispatch(setSignMode(undefined));
+    } else {
+      navigate('/');
+    }
+  };
+
   return (
     <div className={styles.login}>
-      <div className={styles.login__form}>
-        <Button version='blue' square={false} size='md' onClick={setSignInMode}>
-          Войти
-        </Button>
-        <Button version='red' square={false} size='md' onClick={setSignUpMode}>
-          Зарегистрироваться
-        </Button>
-        <Button version='gray-light' square={false} size='md' onClick={signInWithGoogle}>
-          Google Account
+      <div className={styles.login__back}>
+        <Button version='gray-light' square={true} size='md' onClick={goBack}>
+          <img src={ArrowLeft} alt='arrow-left' />
         </Button>
       </div>
-      <form onSubmit={e => signIn(e)} className={styles.login__signContainer}>
-        {signMode === 'signIn' && signMode ? (
-          <>
-            <Input version='text' value={email} onChange={e => setEmail(e.target.value)} />
-            <Input version='text' value={password} onChange={e => setPassword(e.target.value)} />
-            <Button version='gray-light' square={false} size='sm' onClick={signIn}>
-              Войти
-            </Button>
-          </>
-        ) : (
-          <>
-            <Input version='text' value={email} onChange={e => setEmail(e.target.value)} />
-            <Input version='text' value={password} onChange={e => setPassword(e.target.value)} />
-            <Input
-              version='text'
-              value={confirmedPassword}
-              onChange={e => setConfirmPassword(e.target.value)}
-            />
-            <Button version='gray-light' square={false} size='sm' onClick={signUp}>
-              Зарегистрироваться
-            </Button>
-          </>
-        )}
-      </form>
+      {!signMode && (
+        <div className={styles.login__form}>
+          <Button version='blue' square={false} size='md' onClick={setSignInMode}>
+            Войти
+          </Button>
+          <Button version='red' square={false} size='md' onClick={setSignUpMode}>
+            Зарегистрироваться
+          </Button>
+          <Button version='gray-light' square={false} size='md' onClick={signInWithGoogle}>
+            Google Account
+          </Button>
+        </div>
+      )}
+      {signMode && (
+        <form onSubmit={e => signIn(e)} className={styles.login__signContainer}>
+          {signMode === 'signIn' && (
+            <>
+              <Input
+                label='Почта'
+                version='text'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+              <Input
+                label='Пароль'
+                version='text'
+                type='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+              <Button version='gray-light' square={false} size='sm' onClick={signIn}>
+                Войти
+              </Button>
+            </>
+          )}
+          {signMode === 'signUp' && (
+            <>
+              <Input
+                label='Почта'
+                version='text'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+              <Input
+                label='Пароль'
+                version='text'
+                type='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+              <Input
+                label='Повторите пароль'
+                version='text'
+                value={confirmedPassword}
+                type='password'
+                onChange={e => setConfirmPassword(e.target.value)}
+              />
+              <Button version='gray-light' square={false} size='sm' onClick={signUp}>
+                Зарегистрироваться
+              </Button>
+            </>
+          )}
+        </form>
+      )}
     </div>
   );
 };
--- src/components/controls/Input/Input.tsx
+++ src/components/controls/Input/Input.tsx
@@ -5,13 +5,28 @@ import LensGray from '@/assets/icons/lens-gray.svg';
 
 interface Input extends InputHTMLAttributes<HTMLInputElement> {
   version: 'search' | 'number' | 'text';
+  label?: string;
 }
 
-export const Input = ({ version = 'search', ...rest }: Input) => {
+export const Input = ({ version = 'search', label = undefined, ...rest }: Input) => {
   return (
     <div className={`${styles.container} ${styles[`container--${version}`]}`}>
       {version === 'search' && <img src={LensGray} alt='lens' />}
-      <input className={`${styles.input} ${styles[`input--${version}`]}`} {...rest} />
+      <input
+        id={label}
+        className={`
+          ${styles.container__input} 
+          ${styles[`container__input--${version}`]} 
+          ${label ? styles['container__input--label'] : ''}
+        `}
+        placeholder={label}
+        {...rest}
+      />
+      {label && (
+        <label htmlFor={label} className={styles.container__label}>
+          {label}
+        </label>
+      )}
     </div>
   );
 };
